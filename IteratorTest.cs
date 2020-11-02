459c0d74690840b0fa4df6f67dcfd8784ee0bd6d
TACHIBANA.Yushi <y-tachibana@pscsrv.co.jp>
2020-11-02 16:48:23 +0900

    IteratorTest: ジェネリクスを用いたソースを分けて記述

--- IteratorTest.cs
+++ IteratorTest.cs
@@ -6,18 +6,20 @@ namespace DesignPatternTest
     [TestClass]
     public class IteratorTest
     {
+        #region Iterator
+        /*
         [TestMethod]
         public void Iterator()
         {
             BookShelf bookShelf = new BookShelf();
-            bookShelf.appendBook(new Book("Around the world in 80 days"));
-            bookShelf.appendBook(new Book("Bible"));
-            bookShelf.appendBook(new Book("Cinderella"));
-            bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+            bookShelf.AppendBook(new Book("Around the world in 80 days"));
+            bookShelf.AppendBook(new Book("Bible"));
+            bookShelf.AppendBook(new Book("Cinderella"));
+            bookShelf.AppendBook(new Book("Daddy-Long-Legs"));
             IIterator it = bookShelf.Iterator();
             while (it.HasNext())
             {
-                object book = it.Next<Book>();
+                Book book = (Book)it.Next();
                 // Console.WriteLine(book.Name);
             }
         }
@@ -31,13 +33,15 @@ namespace DesignPatternTest
         public interface IIterator
         {
             bool HasNext();
-            object Next<T>();
+
+            object Next();
         }
 
         // 本を表すクラス
         public class Book
         {
             public string Name { get; set; }
+
             public Book(string name)
             {
                 this.Name = name;
@@ -46,18 +50,18 @@ namespace DesignPatternTest
 
         public class BookShelf : IAggregate
         {
-            List<Book> books = new List<Book>();
+            List<Book> Books = new List<Book>();
 
             int Last { get; set; } = 0;
 
             public Book GetBookAt(int index)
             {
-                return books[index];
+                return Books[index];
             }
 
-            public void appendBook(Book book)
+            public void AppendBook(Book book)
             {
-                this.books.Add(book);
+                this.Books.Add(book);
                 Last++;
             }
 
@@ -96,12 +100,115 @@ namespace DesignPatternTest
                 }
             }
 
-            public object Next<T>()
+            public object Next()
             {
                 Book book = BookShelf.GetBookAt(Index);
                 this.Index++;
                 return book;
             }
         }
+        */
+        #endregion
+
+        
+        #region Generics Iterator
+        [TestMethod]
+        public void GenericsIterator()
+        {
+            BookShelf bookShelf = new BookShelf();
+            bookShelf.AppendBook(new Book("Around the world in 80 days"));
+            bookShelf.AppendBook(new Book("Bible"));
+            bookShelf.AppendBook(new Book("Cinderella"));
+            bookShelf.AppendBook(new Book("Daddy-Long-Legs"));
+            IIterator it = bookShelf.Iterator();
+            while (it.HasNext())
+            {
+                object book = it.Next();
+                // Console.WriteLine(book.Name);
+            }
+        }
+
+        public interface IAggregate
+        {
+            IIterator Iterator();
+        }
+
+        // 数え上げ、スキャンを表すインターフェース
+        public interface IIterator
+        {
+            bool HasNext();
+            object Next();
+        }
+
+        // 本を表すクラス
+        public class Book
+        {
+            public string Name { get; set; }
+            public Book(string name)
+            {
+                this.Name = name;
+            }
+        }
+
+        public class BookShelf : IAggregate
+        {
+            List<Book> Books = new List<Book>();
+
+            int Last { get; set; } = 0;
+
+            public Book GetBookAt(int index)
+            {
+                return Books[index];
+            }
+
+            public void AppendBook(Book book)
+            {
+                this.Books.Add(book);
+                Last++;
+            }
+
+            public int GetLength()
+            {
+                return this.Last;
+            }
+
+            public IIterator Iterator()
+            {
+                return new BookShelfIterator(this);
+            }
+        }
+
+        public class BookShelfIterator : IIterator
+        {
+            BookShelf BookShelf { get; set; }
+
+            int Index { get; set; }
+
+            public BookShelfIterator(BookShelf bookShelf)
+            {
+                this.BookShelf = bookShelf;
+                this.Index = 0;
+            }
+
+            public bool HasNext()
+            {
+                if (Index < BookShelf.GetLength())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public object Next()
+            {
+                Book book = BookShelf.GetBookAt(Index);
+                this.Index++;
+                return book;
+            }
+        }
+        #endregion
     }
 }
