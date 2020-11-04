f609f704e59f232725f11554bccdfefc0177e939
TACHIBANA.Yushi <y-tachibana@pscsrv.co.jp>
2020-11-04 10:23:08 +0900

    IteratorTest: インターフェースに型パラメータを追加、while文でイテレータをBook型にキャストする処理を除外できるように修正

--- IteratorTest.cs
+++ IteratorTest.cs
@@ -113,33 +113,37 @@ namespace DesignPatternTest
         
         #region Generics Iterator
         [TestMethod]
-        public void GenericsIterator()
+        public void GenericsIteratorTest()
         {
             BookShelf bookShelf = new BookShelf();
             bookShelf.AppendBook(new Book("Around the world in 80 days"));
             bookShelf.AppendBook(new Book("Bible"));
             bookShelf.AppendBook(new Book("Cinderella"));
             bookShelf.AppendBook(new Book("Daddy-Long-Legs"));
-            IIterator it = bookShelf.Iterator();
+            IIterator<Book> it = bookShelf.Iterator();
             while (it.HasNext())
             {
-                object book = it.Next();
-                // Console.WriteLine(book.Name);
+                Book book = it.Next(); // 問題点：Book型のキャストをなくすには？
+                //Console.WriteLine(book.Name);
             }
         }
 
-        public interface IAggregate
+        #region 共通部品 T型の型パラメータを定義
+        public interface IAggregate<T> // 2.共通部品にT型の型パラメータを定義する
         {
-            IIterator Iterator();
+            IIterator<T> Iterator();
         }
 
         // 数え上げ、スキャンを表すインターフェース
-        public interface IIterator
+        public interface IIterator<T> // 2.共通部品にT型の型パラメータを定義する
         {
             bool HasNext();
-            object Next();
-        }
 
+            T Next(); // 1.Nextメソッドの戻り値をobject型からT型にする
+        }
+        #endregion 共通部品
+
+        #region 本
         // 本を表すクラス
         public class Book
         {
@@ -150,7 +154,7 @@ namespace DesignPatternTest
             }
         }
 
-        public class BookShelf : IAggregate
+        public class BookShelf : IAggregate<Book> // 3.共通部品の型パラメータにBookを入れる
         {
             List<Book> Books = new List<Book>();
 
@@ -172,13 +176,13 @@ namespace DesignPatternTest
                 return this.Last;
             }
 
-            public IIterator Iterator()
+            public IIterator<Book> Iterator()
             {
                 return new BookShelfIterator(this);
             }
         }
 
-        public class BookShelfIterator : IIterator
+        public class BookShelfIterator : IIterator<Book> // 3.共通部品の型パラメータにBookを入れる
         {
             BookShelf BookShelf { get; set; }
 
@@ -202,13 +206,14 @@ namespace DesignPatternTest
                 }
             }
 
-            public object Next()
+            public Book Next()
             {
                 Book book = BookShelf.GetBookAt(Index);
                 this.Index++;
                 return book;
             }
         }
-        #endregion
+        #endregion 本
+        #endregion Iterator
     }
 }
